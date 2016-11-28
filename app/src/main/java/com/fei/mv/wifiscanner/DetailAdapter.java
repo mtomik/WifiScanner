803b8f1707523ff1750ddbd5c707d239c7e502ab
Juraj Vraniak <juvraniak@gmail.com>
2016-11-28 14:39:55 +0100

    fix2

--- app/src/main/java/com/fei/mv/wifiscanner/DetailAdapter.java
+++ app/src/main/java/com/fei/mv/wifiscanner/DetailAdapter.java
@@ -1,19 +1,14 @@
 package com.fei.mv.wifiscanner;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-import com.fei.mv.wifiscanner.model.Record;
-import com.fei.mv.wifiscanner.model.Result;
 import com.fei.mv.wifiscanner.model.WifiScan;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
