1d0983fc181b27214d2c57eba5395dbb30d493e1
mirec <kytkamiroslav@gmail.com>
2016-11-30 23:33:17 +0100

    vylepsenie zistovania polohy

--- app/src/main/java/com/fei/mv/wifiscanner/MainActivity.java
+++ app/src/main/java/com/fei/mv/wifiscanner/MainActivity.java
@@ -23,8 +23,13 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Vector;
+import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -160,16 +165,6 @@ public class MainActivity extends AppCompatActivity {
             scan.add(newOne);
         }
 
-        /*
-        //sortovanie podla signalu
-        List<WifiScan> listOfFindWifi = new1.getWifiScan();
-        Collections.sort(listOfFindWifi, new Comparator<WifiScan>() {
-            @Override
-            public int compare(WifiScan wifi1, WifiScan wifi2) {
-                return Integer.parseInt(wifi1.getRSSI()) > Integer.parseInt(wifi2.getRSSI()) ? -1 : Integer.parseInt(wifi1.getRSSI()) == Integer.parseInt(wifi2.getRSSI()) ? 0 : 1;
-            }
-        });
-        */
         String foundFloor = getTheFloor(scan);
         locationListFragment.setLocationResultText(foundFloor);
     }
@@ -178,24 +173,36 @@ public class MainActivity extends AppCompatActivity {
         //tu vytiahneme vsetky poschodia so vsetkymi wifinami
         List<Record> floors = allRecords;
         Map<String,Integer> scoredFloors = new HashMap<>();
-
+        Map<String,Integer> sortedScoredFloors = new HashMap<>();
         //Toast.makeText(this," z DB nacital "+floors.size()+" poschodi",Toast.LENGTH_SHORT).show();
         List<WifiScan> savedFloorWifi;
-        //List<WifiScan> findFloorWifi = new ArrayList<>();
+        List<WifiScan> findFloorWifi = new ArrayList<>();
 
         //testovacie find wifi
-        /*
+
         for (Record flor:floors){
-            findFloorWifi = flor.getWifiScan();
-            break;
+            if ((flor.getSection()+flor.getFloor()).equals("A2")){
+                findFloorWifi = flor.getWifiScan();
+                break;
+            }
+
         }
-        */
+
         for (Record floor:floors){
             savedFloorWifi = floor.getWifiScan();
-//            int floorScore = compareWifis(findFloorWifi,savedFloorWifi);
-            int floorScore = compareWifis(listOfFindWifi,savedFloorWifi);
+            int floorScore = compareWifis(findFloorWifi,savedFloorWifi);
+            //int floorScore = compareWifis(listOfFindWifi,savedFloorWifi);
             scoredFloors.put(floor.getSection()+floor.getFloor(),floorScore);
         }
+        sortedScoredFloors = sortByComparator(scoredFloors,false);
+        List<Entry<String, Integer>> entryListScores = new LinkedList<Entry<String, Integer>>(sortedScoredFloors.entrySet());
+        String key = new String();
+        if (entryListScores.get(0).getValue() != 0){
+            key = entryListScores.get(0).getKey();
+        }else
+            return "N/A";
+
+        /*
         Integer[] scores = new Integer[floors.size()];
         scoredFloors.values().toArray(scores);
         //zoradit mapu podla score
@@ -204,38 +211,131 @@ public class MainActivity extends AppCompatActivity {
         Collections.reverse(listScores);
 
         //najdenie kluca podla value
-        String key = new String();
+
+        //int counter=0;
+        //Vector<String> vec = new Vector<String>();
+        String helDesc = new String();
         if ((Integer)listScores.get(0) != 0){
             Iterator<Map.Entry<String,Integer>> iter = scoredFloors.entrySet().iterator();
 
             while (iter.hasNext()) {
                 Map.Entry<String,Integer> entry = iter.next();
                 if (entry.getValue().equals(listScores.get(0))) {
+                    //counter++;
                     key = entry.getKey();
+                    //vec.add(key);
+                    //helDesc.concat(key);
                 }
             }
         }else
             return "N/A";
+        */
+        Toast.makeText(this,entryListScores.get(0).getValue().toString()+"  "+entryListScores.get(1).getValue().toString()+"  "+entryListScores.get(2).getValue().toString()+"  "+entryListScores.get(3).getValue().toString(),Toast.LENGTH_LONG).show();
         return key;
     }
 
     public int compareWifis(List<WifiScan> listOfFindWifi, List<WifiScan> savedFloorWifi){
         int score = 0;
+
+        //sortovanie podla signalu
+        //List<WifiScan> listOfFindWifi = new1.getWifiScan();
+
+        Collections.sort(savedFloorWifi, new Comparator<WifiScan>() {
+            @Override
+            public int compare(WifiScan wifi1, WifiScan wifi2) {
+                return Integer.parseInt(wifi1.getRSSI()) > Integer.parseInt(wifi2.getRSSI()) ? -1 : Integer.parseInt(wifi1.getRSSI()) == Integer.parseInt(wifi2.getRSSI()) ? 0 : 1;
+            }
+        });
+
         for (WifiScan wifina:listOfFindWifi){
-            if (isWifiInList(wifina, savedFloorWifi))
-                score++;
+            score = score + isWifiInList(wifina, savedFloorWifi);
+
         }
         return score;
     }
 
-    public boolean isWifiInList(WifiScan wifina, List<WifiScan> savedFloorWifi){
+    public int isWifiInList(WifiScan wifina, List<WifiScan> savedFloorWifi){
+        int signalTolerance = 5;
+        boolean topWifi = true;
         for (WifiScan wifiFromList:savedFloorWifi){
-            if (wifiFromList.getMAC().equals(wifina.getMAC()))
-                return true;
+            if (wifiFromList.getIs_used()==1){
+                if (wifiFromList.getMAC().equals(wifina.getMAC())){
+                     if( Math.abs( Math.abs(Integer.parseInt(wifiFromList.getRSSI())) - Math.abs(Integer.parseInt(wifina.getRSSI())) ) <= signalTolerance ){
+                         if (topWifi){
+                              topWifi = false;
+                              return 4;
+                         }
+                         else
+                            return 2;
+                     }else
+                         return 1;
+                }
+            }
+            topWifi = false;
         }
-        return false;
+        return 0;
     }
 
+    public Map<String, Integer> sortMapByValues(Map<String, Integer> passedMap) {
+        List<String> mapKeys = new ArrayList<>(passedMap.keySet());
+        List<Integer> mapValues = new ArrayList<>(passedMap.values());
+        Collections.sort(mapValues);
+        Collections.sort(mapKeys);
+
+        Map<String, Integer> sortedMap = new HashMap<>();
+
+        Iterator<Integer> valueIt = mapValues.iterator();
+        while (valueIt.hasNext()) {
+            Integer val = valueIt.next();
+            Iterator<String> keyIt = mapKeys.iterator();
+
+            while (keyIt.hasNext()) {
+                String key = keyIt.next();
+                Integer comp1 = passedMap.get(key);
+                Integer comp2 = val;
+
+                if (comp1.equals(comp2)) {
+                    keyIt.remove();
+                    sortedMap.put(key, val);
+                    break;
+                }
+            }
+        }
+        return sortedMap;
+    }
+
+    private Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap, final boolean ascendingOrder)
+    {
+
+        List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(unsortMap.entrySet());
+
+        // Sorting the list based on values
+        Collections.sort(list, new Comparator<Entry<String, Integer>>()
+        {
+            public int compare(Entry<String, Integer> o1,
+                               Entry<String, Integer> o2)
+            {
+                if (ascendingOrder)
+                {
+                    return o1.getValue().compareTo(o2.getValue());
+                }
+                else
+                {
+                    return o2.getValue().compareTo(o1.getValue());
+
+                }
+            }
+        });
+
+        // Maintaining insertion order with the help of LinkedList
+        Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+        for (Entry<String, Integer> entry : list)
+        {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedMap;
+    }
 }
 
 
