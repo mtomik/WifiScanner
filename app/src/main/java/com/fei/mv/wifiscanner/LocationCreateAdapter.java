9c1460be6b1caf6152cf9a0ae0576148d1d937fb
Juraj Vraniak <juvraniak@gmail.com>
2016-12-03 14:40:06 +0100

    prerobeny save

---
+++ app/src/main/java/com/fei/mv/wifiscanner/LocationCreateAdapter.java
@@ -0,0 +1,95 @@
+package com.fei.mv.wifiscanner;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.fei.mv.wifiscanner.model.WifiScan;
+
+import java.util.List;
+
+/**
+ * Created by juraj on 3.12.2016.
+ */
+
+public class LocationCreateAdapter extends ArrayAdapter<WifiScan> implements View.OnClickListener {
+
+    private Context mContext;
+    private List<WifiScan> scanResults;
+
+    private static class ViewHolder {
+        TextView ssidText;
+        TextView macText;
+        CheckBox checkBox;
+    }
+
+    public LocationCreateAdapter(Context context, List<WifiScan> scanResults) {
+        super(context, R.layout.list_item_detail, scanResults);
+        this.scanResults = scanResults;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int position=(Integer) v.getTag();
+        Object object= getItem(position);
+        WifiScan dataModel=(WifiScan)object;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, final ViewGroup parent) {
+
+        final WifiScan dataModel = getItem(position);
+        ViewHolder viewHolder;
+        final View result;
+
+        if (convertView == null) {
+
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.list_item_detail, parent, false);
+            viewHolder.ssidText = (TextView) convertView.findViewById(R.id.ssid);
+            viewHolder.macText = (TextView) convertView.findViewById(R.id.mac);
+            viewHolder.checkBox = (CheckBox) convertView.findViewById(R.id.activateBox);
+
+            // volanie update na db
+            viewHolder.checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dataModel.setIs_used( ((CheckBox)v).isChecked() ? 1 : 0 );
+                    updateScanResult(dataModel);
+//                    sqlHelper.updateWifiScanUsed(record.getSection()+record.getFloor(), dataModel);
+//                    Toast.makeText(mContext, "Zaznam aktualizovany!", Toast.LENGTH_SHORT).show();
+                }
+            });
+            result=convertView;
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+            result=convertView;
+        }
+
+
+        viewHolder.checkBox.setChecked( dataModel.getIs_used() == 1 );
+        viewHolder.ssidText.setText(dataModel.getSSID());
+        viewHolder.macText.setText(dataModel.getMAC());
+
+        return convertView;
+    }
+
+    public void updateScanResult(WifiScan scan){
+        for(WifiScan w : scanResults){
+            if(scan.equals(w)){
+                w.setIs_used(scan.getIs_used());
+                break;
+            }
+        }
+        ((MainActivity)getContext()).scanResults = scanResults;
+    }
+
+
+}
--- app/src/main/java/com/fei/mv/wifiscanner/LocationCreateFragment.java
+++ app/src/main/java/com/fei/mv/wifiscanner/LocationCreateFragment.java
@@ -1,6 +1,5 @@
 package com.fei.mv.wifiscanner;
 
-import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,8 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -20,7 +17,6 @@ import android.widget.Toast;
 import com.fei.mv.wifiscanner.model.Record;
 import com.fei.mv.wifiscanner.model.WifiScan;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -34,10 +30,11 @@ public class LocationCreateFragment extends Fragment implements View.OnClickList
     private Spinner sectionSpinner;
     private Spinner floorSpinner;
     private List<WifiScan> scanResults;
-    private ArrayAdapter<String> infoAdapter;
     private View rootView;
-    private List<String> info;
-
+    private Button saveButton;
+    private Button rescanButton;
+    private ListView listView;
+    private LocationCreateAdapter adapter;
 
 
     @Nullable
@@ -45,13 +42,33 @@ public class LocationCreateFragment extends Fragment implements View.OnClickList
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.location_add, container, false);
 
-        this.scanResults = ((MainActivity)getActivity()).scanResults;
+        this.scanResults = ((MainActivity)getActivity()).startScan();
         this.wifi = ((MainActivity)getActivity()).wifi;
         this.sqlHelper = ((MainActivity)getActivity()).sqlHelper;
 
+        initializeSpinners();
+        initializeButtons();
+
+        listView = (ListView) rootView.findViewById(R.id.listview_wifi) ;
+
+        adapter = new LocationCreateAdapter(getActivity(), scanResults);
+        listView.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    private void initializeButtons() {
+        saveButton = (Button) rootView.findViewById(R.id.save_location);
+        saveButton.setOnClickListener(this);
+        rescanButton = (Button) rootView.findViewById(R.id.rescan_wifi);
+        rescanButton.setOnClickListener(this);
+    }
+
+
+    public void initializeSpinners(){
         sectionSpinner = (Spinner) rootView.findViewById(R.id.section_spinner);
         ArrayAdapter<CharSequence> sectionAdapter = ArrayAdapter.createFromResource(getContext(),
-        R.array.sections, android.R.layout.simple_spinner_item);
+                R.array.sections, android.R.layout.simple_spinner_item);
         sectionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sectionSpinner.setAdapter(sectionAdapter);
 
@@ -60,73 +77,26 @@ public class LocationCreateFragment extends Fragment implements View.OnClickList
                 R.array.floorNum, android.R.layout.simple_spinner_item);
         floorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         floorSpinner.setAdapter(floorAdapter);
-
-        fillInfoList();
-
-        populateListView(rootView);
-        Button saveButton = (Button) rootView.findViewById(R.id.save_location);
-        saveButton.setOnClickListener(this);
-        Button rescanButton = (Button) rootView.findViewById(R.id.rescan_wifi);
-        rescanButton.setOnClickListener(this);
-
-        return rootView;
-    }
-
-    public void populateListView(View rootView){
-        infoAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_wifi ,R.id.wifi_info, info);
-        ListView view = (ListView) rootView.findViewById(R.id.listview_wifi);
-
-        view.setAdapter(infoAdapter);
-    }
-
-    public void fillInfoList(){
-        info = null;
-        if(scanResults != null){
-            info = new ArrayList<String>();
-            for (WifiScan s : scanResults){
-                this.info.add("Mac:" + s.getMAC()+"\nSSID:"+s.getSSID()+"\tLevel:"+s.getRSSI());
-            }
-        }
     }
 
     public void rescan(View v){
-        List<WifiScan> scan = new ArrayList<>();
-
-        wifi.startScan();
-        List<ScanResult> result =  wifi.getScanResults();
-        for (ScanResult one : result){
-            WifiScan newOne = new WifiScan();
-            newOne.setSSID(one.SSID);
-            newOne.setRSSI(String.valueOf(one.level));
-            newOne.setMAC(one.BSSID);
-
-            scan.add(newOne);
-        }
-        scanResults = scan;
-        fillInfoList();
-        infoAdapter.clear();
-        infoAdapter.addAll(info);
-        infoAdapter.notifyDataSetChanged();
+        scanResults = ((MainActivity)getActivity()).startScan();
+        ((MainActivity)getActivity()).scanResults = scanResults;
+        adapter.clear();
+        adapter.addAll(scanResults);
+        adapter.notifyDataSetChanged();
     }
 
     public void save(View v){
-        ListView items = (ListView) getActivity().findViewById(R.id.listview_wifi);
+//        ListView items = (ListView) getActivity().findViewById(R.id.listview_wifi);
 
-        for(int i=0;i<items.getCount();i++){
-            View view = items.getAdapter().getView(i, null, null);
-            CheckBox checkBox = (CheckBox) ((LinearLayout) view).getChildAt(1);
-            if(checkBox.isChecked())
-                scanResults.get(i).setIs_used(1);
-            else
-                scanResults.get(i).setIs_used(0);
-        }
         Record record = new Record();
         String floor = floorSpinner.getSelectedItem().toString();
         String section = sectionSpinner.getSelectedItem().toString();
         record.setFloor(floorSpinner.getSelectedItem().toString());
         record.setSection(sectionSpinner.getSelectedItem().toString());
         record.setEdited_at(new Date());
-        record.setWifiScan(scanResults);
+        record.setWifiScan(((MainActivity)getActivity()).scanResults);
 
         sqlHelper.addLocationRecord(record);
 
